f73871e21dea5b8a25295e1c1e7527df4d5f8ee7
Carlos Tovar <cartovarc@gmail.com>
2019-12-11 03:45:17 -0500

    tested alpha 1.0

--- DBEST.py
+++ DBEST.py
@@ -1,82 +1,114 @@
+#!/usr/bin/python
+
 from flask import Flask
 from threading import Lock
-import serial, serial.tools.list_ports, time, threading
+import serial
+import serial.tools.list_ports
+import time
+import threading
 
 # serial config
-DEVICE_NAME = '/dev/ttyUSB' # it will update in update_device_name
 BAUDRATE = 9600
-TIMEOUT = 1
-MAX_BYTES = 100
+TIMEOUT = 0.5
 
-PREPARE_EXCHANGER = "PREPARE_EXCHANGER"
-DRON_IN = "DRON_IN"
-DRON_OUT = "DRON_OUT"
-GET_STATE = "GET_STATE"
-DEBUG_LED = "LED"
-ALLOWED_MESSAGES = [PREPARE_EXCHANGER, DRON_IN, DRON_OUT, GET_STATE, DEBUG_LED]
-TIMEOUT_ERROR = "TIMEOUT_ERROR"
-INTERNAL_ERROR = "INTERNAL_ERROR"
-COMMUNICATION_ERROR = "COMMUNICATION_ERROR"
-FORBIDDEN_ERROR = "FORBIDDEN_ERROR"
+PREPARE_EXCHANGER = 'PREPARE_EXCHANGER'
+DRON_IN = 'DRON_IN'
+DRON_OUT = 'DRON_OUT'
+GET_STATE = 'GET_STATE'
+DEBUG_LED = '1'
+DEBUG_STATE = '2'
+ALLOWED_MESSAGES = [
+    PREPARE_EXCHANGER,
+    DRON_IN,
+    DRON_OUT,
+    GET_STATE,
+    DEBUG_LED,
+    DEBUG_STATE,
+]
+TIMEOUT_ERROR = 'TIMEOUT_ERROR'
+INTERNAL_ERROR = 'INTERNAL_ERROR'
+NO_SERIAL_PORT = 'NO_SERIAL_PORT'
+FORBIDDEN_ERROR = 'FORBIDDEN_ERROR'
+SENT = 'SENT'
+
+lock = Lock()  # Lock for multiple requests
+app = Flask(__name__)  # app for http server
 
 
-lock = Lock() # Lock for multiple requests
-app = Flask(__name__) # app for http server
+def get_port():
+    try:
+        comports = serial.tools.list_ports.comports()
+        device_name = comports[0].device
+        ser = serial.Serial(device_name, BAUDRATE, timeout=TIMEOUT)
+        return ser
+    except Exception:
+        # print("NOT AVAILBLE DEVICES")
+        return None
 
-def update_device_name():
-    while True:
-        try:
-            comports = serial.tools.list_ports.comports()
-            DEVICE_NAME = comports[0].device
-        except Exception:
-            DEVICE_NAME = None
-        print(DEVICE_NAME)
-        time.sleep(1)
-        
-
-thread = threading.Thread(target=update_device_name, args=[])
-thread.daemon = True
-thread.start()
 
 def send_data(message):
-    try:
-        with serial.Serial(DEVICE_NAME, BAUDRATE, timeout=TIMEOUT) as ser:
-            try:
-                ser.write(bytes(message, "utf-8"))
-            except serial.SerialTimeoutException:
-                return TIMEOUT_ERROR
-            except Exception:
-                return INTERNAL_ERROR
-    except Exception:
-        return COMMUNICATION_ERROR
+    lock.acquire()
+    print('MESSAGE TO SEND: ' + str(message))
+    ser = get_port()
+    print('SERIAL PORT %s' % ser)
+    result = None
+    if ser:
+        try:
+            ser.write(bytes(message, 'utf-8'))
+            result = SENT
+        except serial.SerialTimeoutException:
+            result = TIMEOUT_ERROR
+        except Exception:
+            result = INTERNAL_ERROR
+    else:
+        result = NO_SERIAL_PORT
+    lock.release()
+    return result
+
 
 def receive_data():
-    try:
-        with serial.Serial(DEVICE_NAME, BAUDRATE, timeout=TIMEOUT) as ser:
-            try:
-                received_message = str(ser.read(MAX_BYTES))
-                return receive_data
-            except Exception:
-                return INTERNAL_ERROR
-    except Exception:
-        return COMMUNICATION_ERROR
+    lock.acquire()
+    ser = get_port()
+    result = None
+    if ser:
+        try:
+            received_message = str(ser.readline())
+            result = received_message
+        except Exception:
+            result = INTERNAL_ERROR
+    else:
+        result = NO_SERIAL_PORT
+    lock.release()
+    return result
+
 
 def send_and_receive(message):
-    lock.acquire()
     send_result = send_data(message)
     receive_result = receive_data()
-    lock.release()
+    print('send_result: ' + str(send_result))
+    print('receive_result: ' + str(receive_result))
     return receive_result
 
 
-@app.route('/<message>')                                                                                                   
+@app.route('/<message>')
 def process_message(message):
     if message in ALLOWED_MESSAGES:
-        result = send_and_receive(message)
+        result = send_data(message)
         return result
     else:
         return FORBIDDEN_ERROR
 
+
+def read_state():
+    while True:
+        data = receive_data()
+        print('READ: ' + str(data))
+        time.sleep(0.2)
+
+
+thread = threading.Thread(target=read_state, args=[])
+thread.daemon = True
+thread.start()
 
 if __name__ == '__main__':
     app.run()
